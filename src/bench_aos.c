ac9487808787d849131f3a123fad9925c2bc052c
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2022-01-17 16:11:37 +0100

    Working on ao_bench

--- src/bench_aos.c
+++ src/bench_aos.c
@@ -8,28 +8,30 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <assert.h>
+#include "Alz_small.h"
 
 
-#define WALK_NUM  1000
 int main(int argc, char** argv)
 {
-  const int64_t walk_num = WALK_NUM;
-
-  if (argc < 2){
-    fprintf(stderr,"syntax: %s [FILE]\n", argv[0]);
-    return -1;
-  }
-  char* file_name = argv[1];
-
   qmckl_context context;
   context = qmckl_context_create();
   qmckl_exit_code rc;
 
+  const char* file_name  = alz_small_file_name;
+  const int64_t walk_num = alz_small_walk_num;
+  const int64_t elec_num = alz_small_elec_num;
+
+  printf("Reading %s.\n", alz_small_file_name);
+  rc = qmckl_trexio_read(context, file_name);
+  if (rc != QMCKL_SUCCESS) {
+    printf("%s\n", qmckl_string_of_error(rc));
+  }
+  assert (rc == QMCKL_SUCCESS);
+
   rc = qmckl_set_electron_walk_num(context, walk_num);
   assert (rc == QMCKL_SUCCESS);
 
-
-  rc = qmckl_trexio_read(context, file_name);
+  rc = qmckl_set_electron_coord(context, 'N', alz_small_elec_coord, walk_num*elec_num*3);
   assert (rc == QMCKL_SUCCESS);
 
 
